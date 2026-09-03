b2884901a47e32af4f333aee50559899e3a70598
Slimerduck <platon.parv@tptlive.ee>
2026-09-03 09:56:11 +0300

    math examples

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 $test = 10;
-$test = 3.1;
-$test = 'Hello world!';
-$test = 'õpilane';
-$test = '💩';
-$test = true;
-$test = false;
-$test = null;
-$test = [1, null, true, 'lol', 2, 3.2];
-$test = new stdClass();
-$test->name = 'Kaspar';
+$test = 10 + 10;
+$test = 10 - 10;
+$test = 10 * 10;
+$test = 10 / 10;
+$test = $test + 10;
+$test = 2 ** 3;
+$test = 5 % 2;
+$test += 3;
+$test -= 3;
+$test *= 3;
+$test /= 3;
+$test %= 3;
+$test **= 3;
+$test %= 3;
+$test++;
+$test--;
+--$test;
+++$test;
+$a = 1;
+$b = ++$a; 
+var_dump($b, $a);
+
+$test = sin(pi());
 var_dump($test);
 ?>
