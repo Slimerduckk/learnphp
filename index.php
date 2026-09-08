0c7685ebaed531893102f628eea7137ba6db965c
Slimerduck <platon.parv@tptlive.ee>
2026-09-08 09:23:43 +0300

    array examples

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
 <?php
-$test = 10 >5;
-$test = 10 <5;
-$test = 10 == 10;
-$test = 10 <= 10;
-$test = 10 >= 10;
-$test = 10 == '10';
-$test = 10 === '10';
-$test = 10 != '10';
-$test = 10 !== '10';
-$test = [] == 0;
-//https://www.php.net/manual/en/types.comparisons.php
+$test = Array(1,2,3);
+$test = [1,2,3];
+$test = [1, 'woof:3', true, [1,2,3] ];
+$test = [
+    1,
+    'name' => 'celeste',
+    'age' => 23,
+    3,
+    'meaow:3',
+    true,
+    100 => 'qwq',
+    'O/////O',
+];
+var_dump($test['name']);
+array_push ($test, 'O_O', 'mrow :3',3);
+$test[] = 'new :3';
+$test['name'] = 'Rem';
+$test[1] = 67;
+unset($test[3]);
 var_dump($test);
