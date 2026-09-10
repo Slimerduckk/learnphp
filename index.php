e35d6ce3b3b2d16a52f4e133dd0484dc082cdd30
Slimerduck <platon.parv@tptlive.ee>
2026-09-10 09:33:31 +0300

    class example

--- index.php
+++ index.php
@@ -1,20 +1,28 @@
 <?php
-$test = function(){
-    var_dump('meow');
-};
 
-var_dump($test);
+class Box{
+    public $width;
+    public $height;
+    public $length;
+    public $isOpen = false;
+    public $hasBeenOpened  = false;
 
-$test();
-call_user_func($test);
+    public function open(){
+        $this ->isOpen = true;
+    }
+}
 
-$numbers = [1,2,3,4,5];
+$box1 = new Box();
+$box1 -> width = 10;
+$box1 -> height= 10;
+$box1 -> length = 10;
+$box1 -> open();
+var_dump($box1);
 
-$squares = array_map(function($n){
-    return $n*$n;
-}, $numbers); 
-var_dump($squares);
-
-$squares = array_map(fn ($n) => $n * $n, $numbers);
-var_dump($squares);
-?>
+$box2 = new Box();
+$box2 -> width = 20;
+$box2 -> height= 20;
+$box2 -> length = 20;
+$box2 -> open();
+var_dump($box2);
+var_dump($box1);
