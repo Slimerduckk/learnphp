74cefad719c4d11780c66de2e5c8010e6484c9a0
Slimerduck <platon.parv@tptlive.ee>
2026-09-10 09:13:14 +0300

    functions callback examples

--- index.php
+++ index.php
@@ -1,38 +1,20 @@
 <?php
-function hello(): void {
-    var_dump('Hello');
-}
+$test = function(){
+    var_dump('meow');
+};
 
-var_dump (hello());
-hello();
-hello();
+var_dump($test);
 
-function helloName($name = 'Nameless', $age = 'unknown'){
-    var_dump("hello $name! you are $age years old!");
-}
+$test();
+call_user_func($test);
 
-helloName('celeste', 23);
-helloName('rem', 15);
-helloName();
+$numbers = [1,2,3,4,5];
 
-function square (int $a): int{
-    if($a<0){
-        return 0 ;
-    }
-    return $a * $a;
-}
+$squares = array_map(function($n){
+    return $n*$n;
+}, $numbers); 
+var_dump($squares);
 
-$answer = square(2);
-var_dump($answer);
-var_dump(square(4));
-
-function recursion($i){
-    if($i < 10){
-        var_dump($i);
-        reecursion($i+1);
-    }
-}
-
-recursion(0);
-
+$squares = array_map(fn ($n) => $n * $n, $numbers);
+var_dump($squares);
 ?>
