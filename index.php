7b9cbe2fc2cc5f9a14131de263935a8f2b32d03b
Slimerduck <platon.parv@tptlive.ee>
2026-09-22 09:37:18 +0300

    simple php site with us and index and data

--- index.php
+++ index.php
@@ -1,14 +1,48 @@
-<?php include "partials/header.php"; ?>
-    <main class="container">
-        <?php include "partials/hero.php"; ?>
-        <?php include "partials/featured.php"; ?>
-      <div class="row g-5">
-        <div class="col-md-8">
-          <?php include "partials/posts.php"; ?>
-        </div>
-        <div class="col-md-4">
-            <?php include "partials/sidebar.php"; ?>
-        </div>
-      </div>
-    </main>
-<?php include "partials/footer.php"; ?>
+<?php
+$title = 'World';
+$posts = [
+    [
+        'title' => 'Some World title 1',
+        'date' => 'january 1, 2021',
+        'author' => 'ivy',
+        'body' => 'Some World content 1',
+    ],
+    [
+        'title' => 'Some World title 2',
+        'date' => 'january 9, 2021',
+        'author' => 'paige',
+        'body' => 'Some World content 2',
+    ],
+    [
+        'title' => 'Some World title 3',
+        'date' => 'january 18, 2021',
+        'author' => 'mina',
+        'body' => 'Some World content 3',
+    ],
+    [
+        'title' => 'Some World title 4',
+        'date' => 'january 23, 2021',
+        'author' => 'celeste',
+        'body' => 'Some World content 4',
+    ],
+]
+
+
+
+?>
+
+
+<?php include __DIR__ . '/partials/header.php'; ?>
+<main class="container">
+  <?php include __DIR__ . '/partials/hero.php'; ?>
+  <?php include __DIR__ . '/partials/featured.php'; ?>
+    <div class="row g-5">
+    <div class="col-md-8">
+      <?php include __DIR__ . '/partials/posts.php'; ?>
+    </div>
+    <div class="col-md-4">
+      <?php include __DIR__ . '/partials/sidebar.php'; ?>
+    </div>
+  </div>
+</main>
+<?php include __DIR__ . '/partials/footer.php'; ?>
